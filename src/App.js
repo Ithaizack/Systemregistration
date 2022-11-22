98b2b2377ea7f31386c9567f20149d28bf2b12cc
Ithaizack <isaacabreu499@gmail.com>
2022-11-22 07:35:53 -0300

    Nova estrutura do site

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import LoginRegister from './Pages/LoginRegistration/LoginRegister';
 import Create from './Pages/Create/Create';
 import Views from './Pages/Views/Views';
 import Settings from './Pages/Setting/Settings';
+import Start from './Pages/LoginRegistration/inicio';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LoginRegister/>} />
+        <Route path='/' element={<Start/>} />
+        <Route path='/Login' element={<LoginRegister/>}/>
+        <Route path='/Register' element={<></>}/>
         <Route path='/Home' element={<Home/>} />
         <Route path='/Home/Create' element={<Create/>} />
         <Route path='/Home/Views' element={<Views/>} />
